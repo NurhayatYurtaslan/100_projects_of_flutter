6303c7e2f2b601411751c807409eaed8eb46b414
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-06-18 00:05:45 +0300

    update 🆕: color

--- plant_app_ui/lib/core/constants/color_constants.dart
+++ plant_app_ui/lib/core/constants/color_constants.dart
@@ -2,4 +2,4 @@ import 'package:flutter/material.dart';
 
 Color primaryColor = const Color.fromARGB(255, 249, 119, 132);
 Color secondaryColor = const Color.fromARGB(255, 254, 244, 243);
-Color blackColor = Colors.black;
+
