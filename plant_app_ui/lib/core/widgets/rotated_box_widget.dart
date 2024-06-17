672f6641eeca0230394d5e1c2c5730a57ca59202
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-06-17 20:09:45 +0300

    add ✨: widget

---
+++ plant_app_ui/lib/core/widgets/rotated_box_widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:plant_app_ui/constants/color_constants.dart';
+
+class RotatedBoxWidget extends StatelessWidget {
+  final String widgetText;
+  final bool indexController;
+  const RotatedBoxWidget({
+    super.key,
+    required this.widgetText,
+    required this.indexController,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 14),
+      child: RotatedBox(
+        quarterTurns: 3,
+        child: RichText(
+            text: TextSpan(children: [
+          TextSpan(
+            text: ".",
+            style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                color: indexController ? secondaryColor : primaryColor),
+          ),
+          TextSpan(
+              text: widgetText,
+              style: TextStyle(
+                  color: indexController ? blackColor : primaryColor)),
+        ])),
+      ),
+    );
+  }
+}
